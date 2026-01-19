9d5483b5a9dc2737d77e37ab3e46c3ca90565c18
Alexa-Web-com <aleksandra.wilczynska.plgdy@gmail.com>
2026-01-19 18:23:08 +0100

    resize logo image

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
-import shieldDark from "@/assets/images/shield-dark-64.png";
-import shieldLight from "@/assets/images/shield-light-64.png";
-import logoFullDark from "@/assets/images/wPolisa_agecja_tr-bg_light-text.png";
-import logoFullLight from "@/assets/images/wPolisa_agecja_tr-bg_dark-text.png";
+import shieldDark from "@/assets/images/shield-dark-168.png";
+import shieldLight from "@/assets/images/shield-light-168.png";
+import logoFullDark from "@/assets/images/wPolisa_agecja_tr-bg_light-text_504x168.png";
+import logoFullLight from "@/assets/images/wPolisa_agecja_tr-bg_dark-text_504x168.png";
 import { Icon } from "@/utils/types";
 
 type LogoType = {
@@ -16,7 +16,7 @@ const Logo = ({ logoVariant }: LogoType) => {
   return (
     <Link href="/" className="flex items-center gap-2 transition-opacity hover:opacity-80 focus:outline-none">
       <div
-        className={`flex h-14 min-w-14 items-center justify-center rounded-lg ${isLogoLight ? "bg-basic-bright" : "bg-gradient-hero"}`}
+        className={`flex h-14 min-w-14 w-14 items-center justify-center rounded-lg ${isLogoLight ? "bg-basic-bright" : "bg-gradient-hero"}`}
       >
         <Image
           src={isLogoLight ? shieldDark : shieldLight}
@@ -24,17 +24,17 @@ const Logo = ({ logoVariant }: LogoType) => {
           width={56}
           height={56}
           priority
-          className="h-full w-full object-contain"
+          className="h-full w-auto object-contain"
         />
       </div>
-      <div className={`flex h-14 min-w-42 items-center justify-center`}>
+      <div className={`flex h-14 min-w-42 w-42 items-center justify-center`}>
         <Image
           src={isLogoLight ? logoFullDark : logoFullLight}
           alt="wPolisa - Multiagencja Ubezpieczeniowa"
           width={168}
           height={56}
           priority
-          className="h-full w-full object-contain"
+          className="h-full w-auto object-contain"
         />
       </div>
     </Link>
