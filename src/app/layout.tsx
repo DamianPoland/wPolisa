8deb8baf780db708b899d59840ea78406f1d6784
Damian <damian.wilczynski.pl@gmail.com>
2026-01-21 20:13:29 +0100

    logo change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -81,12 +81,20 @@ export const metadata: Metadata = {
   // Icons
   icons: {
     icon: [
-      { url: "/icons/icon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/icons/icon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/icons/favicon.ico" },
+      { url: "/icons/icon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icons/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/icon-512x512.png", sizes: "512x512", type: "image/png" },
+      { url: "/icons/favicon.ico", sizes: "48x48", type: "image/x-icon" },
     ],
     shortcut: "/icons/favicon.ico",
-    apple: "/icons/apple-touch-icon.png",
+    apple: [
+      {
+        url: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
   },
 
   // GEO & Language
